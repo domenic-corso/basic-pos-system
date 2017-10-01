778180c3f089ebac52c81f9c32fe6f7ead7ee48a
ShopFront Test <Domenic Corso>
2017-10-01 21:48:59 +1100

    Added static validation functions to Product

---
+++ app/Product.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Validator;
+
+class Product extends Model
+{
+	protected $fillable = ['name', 'short_name', 'category_id', 'price_definition_id', 'fixed_price'];
+
+	/* This function takes care of any business logic given an array of properties.
+	This includes:
+		- Rounding numbers to 0.05,
+		- Converting the short_name into uppercase
+		- Deciding between price definition or fixed price */
+	public static function applyProperties (Product $product, array $properties) : Product {
+		$product->fill($properties);
+
+		/* Round fixed price to nearest 5 cents. */
+		$product->fixed_price = round($product->fixed_price / 5, 2) * 5;
+		$product->short_name = strtoupper($product->short_name);
+
+		/* If a price definition is provided, prioritize that and remove the fixed price data. */
+		if (!empty($product->price_definition_id)) {
+			$product->fixed_price = null;
+		}
+		else {
+			$product->price_definition_id = null;
+		}
+
+		return $product;
+	}
+
+	/* Checks a given array of properties against the validator rules as well
+	as some business logic. */
+	public static function validateProduct (array $properties) : bool {
+		$validator = Validator::make($properties, self::getValidatorRules());
+
+		/* If the properties are not valid and no price information at all was given,
+		invalid Product. */
+		if (
+			$validator->fails() ||
+			empty($properties['price_definition_id']) && empty($properties['fixed_price'])
+		) {
+			return false;
+		}
+
+		return true;
+	}
+
+    private static function getValidatorRules () : array {
+    	$namePattern = '/^[a-z\d &\']+$/i';
+
+    	return [
+    		'name' => 'required|unique:products|max:60|filled|regex:'.$namePattern,
+    		'short_name' => 'required|unique:products|max:12|filled|regex:'.$namePattern,
+    		'category_id' => 'required|integer|exists:categories,id',
+    		'price_definition_id' => 'integer|exists:price_definitions,id|nullable',
+    		'fixed_price' => 'numeric|min:0|max:50|nullable'
+    	];
+    }
+}
