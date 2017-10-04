3ade2cfa860371339aea42cbd232208ac1c00506
ShopFront Test <Domenic Corso>
2017-10-05 08:15:26 +1100

    JSON orders can now be processed

--- app/Order.php
+++ app/Order.php
@@ -45,4 +45,79 @@ class Order extends Model
 
         return true;
     }
+
+    /* This will accept a JSON String Order and process it into an associative
+    array of format: [
+        "total": 0.00,
+        "discounted": 0.00,
+        "discount_description": "",
+        "order_items": [
+            {
+                "product_name": "",
+                "size": "",
+                "total": ""
+            }
+        ]
+    ]
+    or NULL if it cannot be processed. */
+    public static function processJsonOrder (string $jsonString) {
+        if (!self::validateJsonOrder($jsonString)) { return false; }
+        $order = json_decode($jsonString);
+        $orderItems = $order->order_items;
+
+        $processed = array();
+
+        $processed["total"] = 0.00;
+        $processed["discounted"] = 0.00;
+        $processed["discount_description"] = "";
+        $processed["order_items"] = array();
+
+        $productList = array();
+
+        /* Process each order item. */
+        foreach ($orderItems as $ordItem) {
+            /* Resolve the product_id attribute of $ordItem to a Product Instance. */
+            $productInstance = Product::find($ordItem->product_id);
+            $productList[] = $productInstance;
+
+            $product_name = $productInstance->name;
+            $size = $ordItem->size;
+
+            $total = 0.00;
+
+            /* If the product is put through as having a fixed size, make the total
+            for this OrderItem as the fixed price of the product. */
+            if ($size === 'fixed') {
+                $total = $productInstance->fixed_price;
+            }
+
+            /* Otherwise if the product is put through and a size is specified,
+            use the size attribute on the products' price definition to get the total
+            for this OrderItem */
+            if (!is_null($productInstance->price_definition)) {
+                $total = $productInstance->price_definition[$ordItem->size];
+            }
+
+            /* Format OrderItem array, readable by the client register. */
+            $processed["order_items"][] = array (
+                "product_name" => $product_name,
+                "size" => $size,
+                "total" => floatval($total)
+            );
+        }
+
+        /* Find the total. */
+        foreach ($processed["order_items"] as $ordItem) {
+            $processed["total"] += $ordItem["total"];
+        }
+
+        /* Calculate promotions. */
+        $promotionResult = Promotions::findPromotions($productList);
+        $processed["discounted"] = $promotionResult["discounted"];
+        $processed["discount_description"] = $promotionResult["description"];
+
+        var_dump($processed);
+
+        return $processed;
+    }
 }
--- tests/Unit/OrderTest.php
+++ tests/Unit/OrderTest.php
@@ -9,9 +9,9 @@ use App\Order;
 
 class OrderTest extends TestCase
 {
-    public function testOrderJsonParse () {
-        /* Should be using a test database here. */
-        $jsonString = '{
+
+    public function getTypicalJsonOrderString () : string {
+        return '{
             "order_items": [
                 {
                     "product_id": 2,
@@ -23,7 +23,18 @@ class OrderTest extends TestCase
                 }
             ]
         }';
+    }
+
+    public function testOrderJsonParse () {
+        /* Should be using a test database here. */
+        $jsonString = self::getTypicalJsonOrderString();
 
         $this->assertTrue(Order::validateJsonOrder($jsonString));
+    }
+
+    public function testOrderProcessJson () {
+        $jsonString = self::getTypicalJsonOrderString();
+
+        $this->assertTrue(is_array(Order::processJsonOrder($jsonString)));
     }
 }
