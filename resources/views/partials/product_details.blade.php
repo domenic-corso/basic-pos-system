d0faea2f6c3e99540dddad26f01d9cb98e67a7ab
ShopFront Test <Domenic Corso>
2017-10-04 19:45:30 +1100

    Finished product details partial

--- resources/views/partials/product_details.blade.php
+++ resources/views/partials/product_details.blade.php
@@ -1,3 +1,18 @@
 <div class="alert alert-warning" style="margin-top: 20px">
     <strong>Product Details</strong>
+    <div>
+        <strong>Full Name: </strong>{{ $product->name }}
+    </div>
+    <div>
+        <strong>Short Name: </strong>{{ $product->short_name }}
+    </div>
+    <div>
+        <strong>Category: </strong>{{ $product->category->name }}
+    </div>
+    <div>
+        <strong>Price: </strong>
+        <div>
+            {!! $product->price_long !!}
+        </div>
+    </div>
 </div>
